2e51716ddca16fb9d40ace95edb0a4058e2c7a5b
tj-tieso <tj.tieso@gmail.com>
2022-05-11 16:14:22 -0500

    inquirer added

---
+++ app.js
@@ -0,0 +1,23 @@
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
+
+// const pageHTML = generatePage(name, github);
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+  });
+
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
+
+//   console.log("Portfolio complete! Checkout index.html to see the output");
+// });
